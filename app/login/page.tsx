ef61a3236fd405d40cf4e30a23a027538a143db5
Naman Soni <namantsoni@gmail.com>
2025-05-11 20:01:10 +0530

    Login page

---
+++ app/login/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function Login() {
+    return (
+        <>
+            <div className="flex h-screen w-full items-center justify-center px-4">
+                <Card className="w-full max-w-md">
+                    <CardHeader>
+                        <CardTitle className="text-3xl">Log in to <span className="text-blue-600 italic">Billiant</span></CardTitle>
+                        <CardDescription>Enter your email below to login to your account</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+                        <form className="flex flex-col gap-y-4">
+                            <div className="flex flex-col gap-y-2">
+                                <Label>Email</Label>
+                                <Input placeholder="name@email.com" />
+                            </div>
+                            <Button>Log in</Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
+    );
+}
