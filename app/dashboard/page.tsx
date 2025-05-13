200feb7b248a174b7f7cc618a0cf36dc7e2480e4
Naman Soni <namantsoni@gmail.com>
2025-05-13 18:41:34 +0530

    Login and Logout functionality

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,17 @@
-export default function Dashboard() {
+import { signOut } from "../utils/auth";
+import { requireUser } from "../utils/hooks"
+
+export default async function Dashboard() {
+    const session = await requireUser();
     return (
         <div>
             <h1>Hello from the dashboard route!</h1>
+            <form action={async () => {
+                "use server";
+                await signOut();
+            }}>
+                <button type="submit">Sign Out</button>
+            </form>
         </div>
     )
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,15 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { signIn } from "../utils/auth";
+import { auth, signIn } from "../utils/auth";
+import { SubmitButton } from "../components/SubmitButtons";
+import { redirect } from "next/navigation";
 
-export default function Login() {
+export default async function Login() {
+    const session = await auth();
+    if(session?.user) {
+        redirect("/dashboard");
+    }
     return (
         <>
             <div className="flex h-screen w-full items-center justify-center px-4">
@@ -22,7 +28,7 @@ export default function Login() {
                                 <Label>Email</Label>
                                 <Input name="email" type="email" placeholder="johndoe@email.com" required/>
                             </div>
-                            <Button>Log in</Button>
+                            <SubmitButton />
                         </form>
                     </CardContent>
                 </Card>
